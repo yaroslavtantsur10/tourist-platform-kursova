c0eb75d379d5a8d924967659d3f6b3a8d9aa1e52
yaroslavtantsur10 <yaroslav.tantsur@nure.ua>
2026-06-08 17:14:00 +0100

    Delete src directory

--- src/Helpers/Helpers/RoleNames.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace TourRecommenderPlatform.Helpers;
-
-public static class RoleNames {
-  public const string Administrator = "Administrator";
-
-  public const string User = "User";
-}
--- src/Pages/Pages/Index.cshtml.cs
+++
@@ -1,8 +0,0 @@
-using Microsoft.AspNetCore.Mvc.RazorPages;
-
-namespace TourRecommenderPlatform.Pages;
-
-public class IndexModel : PageModel {
-  public void OnGet() {
-  }
-}
