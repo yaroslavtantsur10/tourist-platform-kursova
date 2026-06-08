3eeb1a0e23d051a37b780d3da81b43ea9245063c
yaroslavtantsur10 <yaroslav.tantsur@nure.ua>
2026-06-08 17:07:44 +0100

    Add files via upload

---
+++ src/Helpers/Helpers/DecimalModelBinder.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+using System.Globalization;
+
+namespace TourRecommenderPlatform.Infrastructure.ModelBinders;
+
+public class DecimalModelBinder : IModelBinder {
+  public Task BindModelAsync(ModelBindingContext bindingContext) {
+    if (bindingContext == null) {
+      throw new ArgumentNullException(nameof(bindingContext));
+    }
+
+    var valueProviderResult = bindingContext.ValueProvider.GetValue(bindingContext.ModelName);
+
+    if (valueProviderResult == ValueProviderResult.None) {
+      return Task.CompletedTask;
+    }
+
+    bindingContext.ModelState.SetModelValue(bindingContext.ModelName, valueProviderResult);
+
+    var value = valueProviderResult.FirstValue;
+
+    if (string.IsNullOrWhiteSpace(value)) {
+      return Task.CompletedTask;
+    }
+
+    value = value.Trim();
+
+    var normalizedValue = value.Replace(',', '.');
+
+    if (decimal.TryParse(
+        normalizedValue,
+        NumberStyles.Number,
+        CultureInfo.InvariantCulture,
+        out var result)) {
+      bindingContext.Result = ModelBindingResult.Success(result);
+    } else {
+      bindingContext.ModelState.TryAddModelError(
+        bindingContext.ModelName,
+        "Введіть коректне числове значення.");
+    }
+
+    return Task.CompletedTask;
+  }
+}
